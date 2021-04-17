0e5166f65d93e3736b14747693481c3c0a4c5823
josur <josuraad@gmail.com>
2021-04-18 01:09:02 +0200

    Ajout 'todo.txt' et complétion du controller

--- site/src/Controller/SandboxController.php
+++ site/src/Controller/SandboxController.php
@@ -5,6 +5,7 @@
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
     /**
      * @Route("/sandbox")
@@ -12,12 +13,28 @@
     class SandboxController extends AbstractController
     {
         /**
-         * @Route("/", name="sandbox")
+         * @Route("/", name="sandbox_index")
          */
-        public function index(): Response
+        public function indexAction(): Response
         {
-            return $this->render("Sandbox/index.html.twig");
+            return $this->render("accueil.html.twig");
         }
 
+
+
+
+
+
+
+        /**
+         * @Route ("/{path}", name="sandbox_error")
+         *
+         *
+         * Permet de générer une erreur 404 pour toutes les pages inconnues
+         */
+        public function errorAction($path): Response
+        {
+            throw new NotFoundHttpException("page inconnue : '$path' ");
+        }
 
     }
