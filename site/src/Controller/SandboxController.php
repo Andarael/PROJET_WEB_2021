19eaef9b723bbe9edd6f4abd674e085da230c281
josur <josuraad@gmail.com>
2021-04-17 23:50:59 +0200

    Initial commit

---
+++ site/src/Controller/SandboxController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SandboxController extends AbstractController
+{
+    /**
+     * @Route("/sandbox", name="sandbox")
+     */
+    public function index(): Response
+    {
+        return $this->render('sandbox/index.html.twig', [
+            'controller_name' => 'SandboxController',
+        ]);
+    }
+}
